77fcf4fdd82fad50c2aa901aca9b200bbfa98330
Cory R Siebler <cory.siebler@gmail.com>
2015-04-05 03:02:21 -0700

    Added YouTube RESTful service

---
+++ YouTubeService/App_Code/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+/// Name:   Cory Siebler
+/// ASUID:  1000832292
+/// Email:  csiebler@asu.edu
+/// Class:  ASU CSE 445 (#11845)
+[ServiceContract]
+public interface IService
+{
+
+}
---
+++ YouTubeService/App_Code/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+/// Name:   Cory Siebler
+/// ASUID:  1000832292
+/// Email:  csiebler@asu.edu
+/// Class:  ASU CSE 445 (#11845)
+public class Service : IService
+{
+
+}
