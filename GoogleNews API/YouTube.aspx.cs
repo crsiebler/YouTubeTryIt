8f984b9180ee24dcbee0d23594c525fe3aefa013
Cory R Siebler <cory.siebler@gmail.com>
2015-04-05 02:41:20 -0700

    Added YouTube RESTful service

--- GoogleNews API/YouTube.aspx.cs
+++ GoogleNews API/YouTube.aspx.cs
@@ -14,6 +14,10 @@ using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 
+/// Name:   Cory Siebler
+/// ASUID:  1000832292
+/// Email:  csiebler@asu.edu
+/// Class:  ASU CSE 445 (#11845)
 namespace YouTube_API
 {
     public partial class YouTubeSearch : System.Web.UI.Page
@@ -36,66 +40,6 @@ namespace YouTube_API
         [WebMethod]
         public static Video[] GetVideos(string topic)
         {
-            /*
-            // Initialize the List of News Articles
-            List<Video> videos = new List<Video>();
-
-            // Initialize the Google News RSS Feed request
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(
-                "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + topic.Replace(" ", "+") + "&type=video&maxResults=10&key=AIzaSyBAKxqcK17_ngThmKCHFLw4g1_aB1eOHDY"
-            );
-
-            // Specify GET Method Request
-            request.Method = "GET";
-
-            // Perform the Request
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            // Check the Response State Code for Success
-            if (response.StatusCode == HttpStatusCode.OK)
-            {
-                Stream receiveStream = response.GetResponseStream();
-                StreamReader readStream = null;
-
-                // Check the Character Set of the Response
-                if (response.CharacterSet == "")
-                {
-                    readStream = new StreamReader(receiveStream);
-                }
-                else
-                {
-                    readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
-                }
-
-                // Convert the Stream in a JSON string
-                string data = readStream.ReadToEnd();
-
-                // Initialize a DataSet to store the parsed information
-                DataSet ds = new DataSet();
-                StringReader reader = new StringReader(data);
-                ds.ReadXml(reader);
-                DataTable articleTable = new DataTable();
-
-                // Make sure the response is not empty
-                if (ds.Tables.Count > 0)
-                {
-                    articleTable = ds.Tables["item"];
-
-                    // Loop through each RSS element and store it as a NewsArticle
-                    foreach (DataRow row in articleTable.Rows)
-                    {
-                        Video video = new Video();
-                        video.id = row["id"]["videoId"].ToString(); 
-                        video.title = row["snippet"]["title"].ToString();
-                        video.date = row["snippet"]["publishedAt"].ToString();
-                        video.description = row["snippet"]["description"].ToString();
-                        videos.Add(video);
-                    }
-                }
-            }
-
-            return videos.ToArray();
-            */
             List<Video> videos = new List<Video>();
 
             var youTubeService = new YouTubeService(new BaseClientService.Initializer()
